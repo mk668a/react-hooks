5e785a6baae319894778957e69cfb23bce3a5fb6
mk668a <msftoaa@gmail.com>
2022-07-24 01:34:58 +0900

    useRef

--- src/pages/useRef.tsx
+++ src/pages/useRef.tsx
@@ -1,11 +1,67 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useRef } from "react";
 import { hooks } from "../constants";
 import { Layout } from "../layout/layout";
 
 export const UseRef: FunctionComponent = () => {
+  const [width, setWith] = useState<number>(0);
+
+  const refElement = useRef<HTMLDivElement>(null);
+
   return (
-    <Layout title={hooks.useRef}>
-      <></>
+    <Layout title={hooks.useRef} code={code}>
+      <>
+        <div
+          className="refElement"
+          ref={refElement}
+          style={{
+            width: "100px",
+            height: "100px",
+            background: "gray",
+          }}
+        ></div>
+        <h2>ref element width: {width}</h2>
+        <button
+          onClick={() => {
+            if (refElement.current) setWith(refElement.current.clientWidth);
+          }}
+        >
+          Set ref element width
+        </button>
+      </>
     </Layout>
   );
 };
+
+const code = `
+const UseRef = () => {
+  const [width, setWith] = useState(0);
+
+  const refElement = useRef(null);
+
+  return (
+    <>
+      <div
+        className="refElement"
+        ref={refElement}
+        style={{
+          width: "100px",
+          height: "100px",
+          background: "gray",
+        }}
+      ></div>
+      <h2>ref element width: {width}</h2>
+      <button
+        onClick={() => {
+          if (refElement.current) setWith(refElement.current.clientWidth);
+        }}
+      >
+        Set ref element width
+      </button>
+    </>
+  );
+};
+
+render(
+  <UseRef />
+)
+`.trim();
